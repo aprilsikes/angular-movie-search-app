bfdd4923fbb2996f829b5debea971652ea7970de
april sikes <aprilsikes@gmail.com>
2016-02-24 13:32:10 -0700

    working api call

--- js/controllers/main_controller.js
+++ js/controllers/main_controller.js
@@ -1,3 +1,13 @@
-app.controller('MainController', ['$scope', function ($scope) {
-  
+app.controller('MainController', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+
+  $scope.movieData;
+
+  $scope.getMovies = function (movieTitle) {
+    $http.get('http://www.omdbapi.com/?s='+movieTitle).then(function(data){
+      console.log(data);
+      var s = $routeParams.s
+      $scope.movieData = data.data.results;
+      return;
+    });
+  }
 }])
