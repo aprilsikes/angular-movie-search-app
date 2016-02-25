473eb67d7c6e1bb95216ba6f09988b268bfa08c3
april sikes <aprilsikes@gmail.com>
2016-02-24 21:27:04 -0700

    working on getting show page

--- js/controllers/show_controller.js
+++ js/controllers/show_controller.js
@@ -1,3 +1,12 @@
 app.controller('ShowController', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
 
+  $scope.movie = [];
+
+  $scope.showMovie = function ({{movie.imdbID}}) {
+    $http.get('http://www.omdbapi.com/?i='+$routeParams.id).then(function(data){
+      $scope.movie = data;
+      console.log($scope.movie);
+      return;
+    });
+  }
 }])
